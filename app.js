1584e3933811562f426d53eddbc2d3f1ccae300a
noahhimself <Noah@Noahs-MacBook-Pro-m.local>
2018-02-28 20:11:16 -0500

    Test7

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mg = require('mailgun');
+var mg = require('mailgun-js');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
